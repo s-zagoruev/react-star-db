e244216ec5094b1e6b1f8ac33ecc25745014cebd
s-zagoruev <zagoruev@gmail.com>
2021-06-07 21:45:19 +0300

    Add several error boundaries component PeoplePage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,18 +2,16 @@ import React, {Component} from 'react'
 
 import Header from '../header'
 import RandomPlanet from '../random-planet'
-import ItemList from '../item-list'
-import PersonDetails from '../person-details'
 
 import './app.css'
 import ErrorButton from "../error-button"
 import ErrorIndicator from "../error-indicator"
+import PeoplePage from "../people-page"
 
 export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: null,
     hasError: false,
   }
 
@@ -22,12 +20,6 @@ export default class App extends Component {
       return {
         showRandomPlanet: !state.showRandomPlanet
       }
-    })
-  }
-
-  onPersonSelected = (id) => {
-    this.setState({
-        selectedPerson: id
     })
   }
 
@@ -41,26 +33,21 @@ export default class App extends Component {
         return <ErrorIndicator/>
     }
 
-    const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null
+    const randomPlanet = this.state.showRandomPlanet ? <RandomPlanet/> : null
 
     return (
       <div className="app">
         <Header/>
-        {planet}
+        {randomPlanet}
         <button
           className="toggle-planet btn btn-warning btn-lg"
           onClick={this.toggleRandomPlanet}>
           Toggle Random Planet
         </button>
         <ErrorButton/>
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onPersonSelected={this.onPersonSelected}/>
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
-        </div>
+        <PeoplePage/>
+        <PeoplePage/>
+        <PeoplePage/>
       </div>
     )
   }
--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './person-details.css'
 import SwapiService from "../../services/swapi-service"
+import ErrorButton from "../error-button"
 
 export default class PersonDetails extends Component {
 
@@ -62,6 +63,7 @@ export default class PersonDetails extends Component {
               <span>{eyeColor}</span>
             </li>
           </ul>
+          <ErrorButton/>
         </div>
       </div>
     )
