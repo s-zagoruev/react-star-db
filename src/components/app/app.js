5330e7439b34e2078dacc966ae9f7631a349124b
s-zagoruev <zagoruev@gmail.com>
2021-06-16 18:31:48 +0300

    Move children from <PersonList>/<PlanetList>/<StarshipList> to HOC withChildFunction

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -47,17 +47,11 @@ export default class App extends Component {
         <PlanetDetails itemId={3}/>
         <StarshipDetails itemId={5}/>
 
-       <PersonList>
-         {({name}) => <span>{name}</span>}
-       </PersonList>
+       <PersonList/>
 
-       <PlanetList>
-         {({name}) => <span>{name}</span>}
-       </PlanetList>
+       <PlanetList />
 
-       <StarshipList>
-         {({name}) => <span>{name}</span>}
-       </StarshipList>
+       <StarshipList/>
 
       </div>
     )
--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -3,15 +3,37 @@ import {withData} from '../hoc-helpers'
 import SwapiService from "../../services/swapi-service"
 
 const swapiService = new SwapiService()
+
 const {
   getAllPeople,
   getAllPlanets,
   getAllStarships
 } = swapiService
 
-const PersonList = withData(ItemList, getAllPeople)
-const PlanetList = withData(ItemList, getAllPlanets)
-const StarshipList = withData(ItemList, getAllStarships)
+const withChildFunction = (View, fn) => {
+  return (props) => {
+    return (
+      <View {...props}>
+        {fn}
+      </View>
+    )
+  }
+}
+
+const renderName = ({name}) => <span>{name}</span>
+const renderModelAndName = ({model, name}) => <span>{name} ({model})</span>
+
+const PersonList = withData(
+  withChildFunction(ItemList, renderName),
+  getAllPeople)
+
+const PlanetList = withData(withChildFunction(
+  ItemList, renderName),
+  getAllPlanets)
+
+const StarshipList = withData(withChildFunction(
+  ItemList, renderModelAndName),
+  getAllStarships)
 
 export {
   PersonList,
