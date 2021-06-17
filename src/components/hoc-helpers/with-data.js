c4b2f5d57b4fd63e431160b5b25a040108bdabc2
s-zagoruev <zagoruev@gmail.com>
2021-06-18 01:27:32 +0300

    Add loading and error states to withData HOC

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -1,10 +1,13 @@
 import {Component} from "react"
 import Spinner from "../spinner"
+import ErrorIndicator from "../error-indicator"
 
 const withData = (View) => {
   return class extends Component {
     state = {
-      data: null
+      data: null,
+      loading: false,
+      error: false
     }
 
     componentDidMount() {
@@ -17,19 +20,37 @@ const withData = (View) => {
       }
     }
 
-
     update() {
+      this.setState({
+        loading: true,
+        error: false
+      })
       this.props.getData()
         .then((data) => {
-          this.setState({data})
+          this.setState({
+            data,
+            loading: false,
+          })
+        })
+        .catch(() => {
+          this.setState({
+            error: true,
+            loading: false
+          })
         })
     }
 
     render() {
-      const {data} = this.state
-      if (!data) {
+      const {data, loading, error} = this.state
+
+      if (loading || !data) {
         return <Spinner/>
       }
+
+      if (error) {
+          return <ErrorIndicator/>
+      }
+
       return (
         <View {...this.props} data={data}/>
       )
