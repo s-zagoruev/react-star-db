62f01d06eb58e8b04e7301b0562d6a986a23f1c4
s-zagoruev <zagoruev@gmail.com>
2021-06-19 17:23:53 +0300

    propTypes

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -9,7 +9,18 @@ export default class RandomPlanet extends Component {
   static defaultProps = {
     updateInterval: 15000
   }
+  
+  static propTypes = {
+    updateInterval: (props, propName, componentName) => {
+      const value = props[propName]
+      if (typeof value === 'number' && !isNaN(value)) {
+          return null
+      }
 
+      return new TypeError(`${componentName}: ${propName} must be number`)
+    }
+  }
+  
   swapiService = new SwapiService()
 
   state = {
